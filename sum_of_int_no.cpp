76e3b7e71abd2416d217d2c1691102e9e18e013f
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-01 16:07:41 +0530

    Create sum_of_int_no.cpp

---
+++ sum_of_int_no.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    int sum = 0;
+
+    cout<<"Enter a number: ";
+    cin>>number;
+
+    while (number != 0)
+    {
+        sum = sum + number;
+        cout<<"Enter a number: ";
+        cin>>number;
+    }
+
+    cout<<"\nsum is: "<<sum<<endl;
+
+    return 0;
+    
+}
