e3f8a132e58cfd5271f1b4860afe4983fbeed66e
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-01 17:20:58 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cout<<"Enter 2 numbers: ";
+    cin>>n1>>n2;
+
+    char op;
+    cout<<"Enter an operator: ";
+    cin>>op;
+
+    switch (op)
+    {
+    case '+':
+        cout<<n1 + n2<<endl;
+        break;
+    case '-':
+        cout<<n1 - n2<<endl;
+        break;
+    case '*':
+        cout<<n1 * n2<<endl;
+        break;
+    case '/':
+        cout<<n1 / n2<<endl;
+        break;         
+    default:
+        cout<<"Invalud operator";
+        break;
+    }
+    return 0;
+}
