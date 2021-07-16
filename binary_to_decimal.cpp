1e4386ae76459c6780c0f61339376f15328c74b4
Asmita Sethiya <77527315+AsmitaSethiya@users.noreply.github.com>
2021-07-16 11:35:11 +0530

    Create binary_to_decimal.cpp

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int binaryToDecimal(int n)
+{
+    int ans = 0;
+    int x = 1;
+
+    while(n != 0)
+    {
+        int y = n % 10;
+        ans = ans + (x * y);
+         x = x * 2;
+         n = n / 10; 
+    }
+
+
+    return ans;
+    
+}
+
+int main()
+{
+    int n;
+
+    cout<<"Enter value of n: ";
+    cin>>n;
+
+    cout<< binaryToDecimal(n) << endl;
+}
